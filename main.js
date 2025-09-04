af91f8211c37d70081fdc781dab2080a7ca87e5b
boibob <155044205+boibob69@users.noreply.github.com>
2025-09-03 23:29:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var clickmeTjiong = document.getElementById("ClickMe");
+var changeToBlack = document.getElementById("ClickMee");
+
+function changeColorToWhite(){
+    document.body.style.backgroundColor = "white";
+}
+
+function changeColorToBlack(){
+    document.body.style.backgroundColor = "black";
+}
